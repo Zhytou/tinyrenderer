da75011935a9660beb23e1abe126cfab35d98ba1
Zhytou <Zhoytou@gmail.com>
2026-06-11 13:52:03 +0800

    add default texture for .mtl

--- src/material.cpp
+++ src/material.cpp
@@ -1,5 +1,7 @@
 #include "material.hpp"
 
+#include "utils.hpp"
+
 namespace tinyrenderer {
 
 Material::Material(const std::filesystem::path& baseDir, const tinyobj::material_t& material) {
@@ -7,18 +9,34 @@ Material::Material(const std::filesystem::path& baseDir, const tinyobj::material
 
     m_name    = material.name;
     m_opacity = material.dissolve;
-    m_albedo  = Texture::create(baseDir / material.diffuse_texname, GL_RGBA8);
-    m_normal  = Texture::create(baseDir / material.normal_texname, GL_RGBA8);
-    m_mrao    = Texture::create(
-        {
-            baseDir / material.metallic_texname,
-            baseDir / material.roughness_texname,
-            baseDir / material.ambient_texname,
-        },
-        GL_TEXTURE_2D,
-        GL_RGBA8,
-        1
-    );
+
+    if (!material.diffuse_texname.empty()) {
+        m_albedo = Texture::create(baseDir / material.diffuse_texname, GL_RGBA8);
+    } else {
+        m_albedo = Texture::create({material.diffuse[0], material.diffuse[1], material.diffuse[2], 1.0f}, GL_RGBA8);
+    }
+
+    if (!material.normal_texname.empty()) {
+        m_normal = Texture::create(baseDir / material.normal_texname, GL_RGBA8);
+    } else {
+        m_normal = Texture::create({0.f, 0.f, 0.f, 0.f}, GL_RGBA8);
+    }
+
+    if (!material.metallic_texname.empty() && !material.roughness_texname.empty() && !material.ambient_texname.empty()) {
+        m_mrao = Texture::create(
+            {
+                baseDir / material.metallic_texname,
+                baseDir / material.roughness_texname,
+                baseDir / material.ambient_texname,
+            },
+            GL_TEXTURE_2D,
+            GL_RGBA8,
+            1
+        );
+    } else {
+        // TODO: .mtl file default ambient value support
+        m_mrao = Texture::create({material.metallic, material.roughness, 0.f, 0.f}, GL_RGBA8);
+    }
 }
 
 }  // namespace tinyrenderer
