0e17de24ca216d8df495bb72176ca8b9be5695dd
Zhytou <Zhoytou@gmail.com>
2026-06-12 17:17:35 +0800

    fix model bounding box calculation

--- include/model.hpp
+++ include/model.hpp
@@ -16,6 +16,8 @@ namespace tinyrenderer {
 struct alignas(16) ModelBlock {
     glm::mat4 transformMatrix;  // translate, rotate, scale
     glm::mat4 normalMatrix;     // transpose of inverse (roate * scale) matrix
+    // glm::mat4 padding1;
+    // glm::mat4 padding2;
 };
 
 class Model {
@@ -28,19 +30,33 @@ class Model {
     Model(const std::filesystem::path& baseDir, const std::filesystem::path& modelName, const glm::mat4& transform);
     ~Model();
 
-    const std::pair<glm::vec3, glm::vec3>& getBoundingBox() const { return m_mesh->getBoundingBox(); }
+    const std::pair<glm::vec3, glm::vec3>& getBoundingBox() const { return m_xyz; }
     const ModelBlock& getModelBlock() const { return m_modelBlock; }
     void getRenderQueue(std::vector<RenderItem>& queue, bool opaque) const;
     void setTransformMatrix(const glm::mat4& transform) {
+        // update transform matrix
         m_modelBlock.transformMatrix = transform;
         m_modelBlock.normalMatrix    = glm::mat4(glm::transpose(glm::inverse(glm::mat3(transform))));
+
+        // update bounding box
+        auto [xyz1, xyz2] = m_mesh->getBoundingBox();
+        xyz1              = glm::vec3(transform * glm::vec4(xyz1, 1.f));
+        xyz2              = glm::vec3(transform * glm::vec4(xyz2, 1.f));
+        m_xyz             = {xyz1, xyz2};
     }
 
    private:
     std::vector<std::shared_ptr<Material>> m_materials;
     std::unique_ptr<Mesh> m_mesh;
 
-    ModelBlock m_modelBlock = {glm::mat4(1.f), glm::mat4(1.f)};
+    std::pair<glm::vec3, glm::vec3> m_xyz = {
+        glm::vec3(FLT_MAX),
+        glm::vec3(-FLT_MAX),
+    };
+    ModelBlock m_modelBlock = {
+        .transformMatrix = glm::mat4(1.f),
+        .normalMatrix    = glm::mat4(1.f),
+    };
 };
 
 }  // namespace tinyrenderer
