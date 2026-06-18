38932e95a1f6125e166898c187118f5718b3c97a
Zhytou <Zhoytou@gmail.com>
2026-06-18 14:29:20 +0800

    add callback and ui support 1、支持运行时通过方向键更改观测视角； 2、支持运行时通过鼠标右键拖动观测视角； 3、支持通过鼠标滚轮放大或缩小视角； 4、通过imgui支持右上角HUD显示，输出当前相机状态、FPS等信息。

--- include/application.hpp
+++ include/application.hpp
@@ -23,15 +23,26 @@ class Application {
 
     // load scene(model/material/light/camera)
     void load(const std::string& scenePath);
-
     // run application main loop
     void run();
 
    private:
+    void processInput(float deltaTime);
+    void drawHUD(float deltaTime);
+    float calculateFPS(float deltaTime);
+
+    static void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+    static void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+    static void cursorPosCallback(GLFWwindow* window, double xpos, double ypos);
+    static void frameSizeCallback(GLFWwindow* window, int width, int height);
+
     GLFWwindow* m_window;
     Renderer m_renderer;
     Scene m_scene;
     uint32_t m_width, m_height;
+
+    bool m_isMouseDragging = false;
+    double m_lastX, m_lastY;
 };
 
 }  // namespace tinyrenderer
--- src/application.cpp
+++ src/application.cpp
@@ -1,6 +1,9 @@
 
 #include "application.hpp"
 
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
 #include <rapidjson/document.h>
 
 #include <filesystem>
@@ -42,7 +45,7 @@ Application::~Application() {
 void Application::setup(const std::string& title) {
     // glfw initialization
     if (!glfwInit()) {
-        throw std::runtime_error("Failed to initialize GLFW library");
+        throw std::runtime_error("Application::setup: Failed to initialize GLFW library");
     }
     // set opengl version
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -55,7 +58,7 @@ void Application::setup(const std::string& title) {
     // glfw window creation
     m_window = glfwCreateWindow(m_width, m_height, title.c_str(), nullptr, nullptr);
     if (!m_window) {
-        throw std::runtime_error("Failed to create OpenGL context");
+        throw std::runtime_error("Application::setup: Failed to create OpenGL context");
     }
 
     // glfw context settings(make sure it is all set before glad initialization)
@@ -66,17 +69,30 @@ void Application::setup(const std::string& title) {
     // set window pointer for later use in callback function
     glfwSetWindowUserPointer(m_window, this);
     // set callback function
-    glfwSetWindowSizeCallback(m_window, [](GLFWwindow* window, int w, int h) {
-        glViewport(0, 0, w, h);
-    });
+    glfwSetScrollCallback(m_window, Application::scrollCallback);
+    glfwSetMouseButtonCallback(m_window, Application::mouseButtonCallback);
+    glfwSetFramebufferSizeCallback(m_window, Application::frameSizeCallback);
+    glfwSetCursorPosCallback(m_window, Application::cursorPosCallback);
 
     // glad initialization(load all OpenGL function pointers)
     if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
-        throw std::runtime_error("Failed to initialize OpenGL extensions loader");
+        throw std::runtime_error("Application::setup: Failed to initialize OpenGL extensions loader");
     }
+
+    // imgui initialization
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(m_window, true);
+    ImGui_ImplOpenGL3_Init("#version 410");
 }
 
 void Application::shutdown() {
+    // destroy imgui context
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    // destroy glfw window
     if (m_window) {
         glfwDestroyWindow(m_window);
     }
@@ -86,21 +102,189 @@ void Application::shutdown() {
 void Application::load(const std::string& scenePath) {
     std::ifstream file{scenePath};
     if (!file.is_open()) {
-        throw std::runtime_error("Could not open file: " + scenePath);
+        throw std::runtime_error("Application::load: Could not open file: " + scenePath);
     }
     std::stringstream buffer;
     buffer << file.rdbuf();
 
     m_scene.initialize(buffer.str());
+    m_renderer.prepare(m_scene);
 }
 
 void Application::run() {
-    m_renderer.prepare(m_scene);
+    float lastFrame = static_cast<float>(glfwGetTime());  // in seconds
     while (!glfwWindowShouldClose(m_window)) {
+        float currFrame = static_cast<float>(glfwGetTime());  // in seconds
+        float deltaTime = currFrame - lastFrame;
+        lastFrame       = currFrame;
+
+        // Process input
+        processInput(deltaTime);
+
+        // Update and render scene
+        m_renderer.update(m_scene);
         m_renderer.render(m_scene);
+
+        // Draw head-up display
+        drawHUD(deltaTime);
+
+        // Swap buffers and poll events
         glfwSwapBuffers(m_window);
         glfwPollEvents();
     }
+}
+
+void Application::processInput(float deltaTime) {
+    auto& camera = m_scene.getCamera();
+
+    if (glfwGetKey(m_window, GLFW_KEY_W) == GLFW_PRESS || glfwGetKey(m_window, GLFW_KEY_UP) == GLFW_PRESS) {
+        camera->move(CameraMovement::UPWARD, deltaTime);
+    }
+    if (glfwGetKey(m_window, GLFW_KEY_S) == GLFW_PRESS || glfwGetKey(m_window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+        camera->move(CameraMovement::DOWNWARD, deltaTime);
+    }
+    if (glfwGetKey(m_window, GLFW_KEY_A) == GLFW_PRESS || glfwGetKey(m_window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+        camera->move(CameraMovement::LEFT, deltaTime);
+    }
+    if (glfwGetKey(m_window, GLFW_KEY_D) == GLFW_PRESS || glfwGetKey(m_window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+        camera->move(CameraMovement::RIGHT, deltaTime);
+    }
+
+    if (glfwGetKey(m_window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+        camera->reset();
+    }
+    if (glfwGetKey(m_window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(m_window, true);
+    }
+}
+
+void Application::drawHUD(float deltaTime) {
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    {
+        const float padding   = 10.0f;
+        ImVec2 windowPos      = ImVec2(static_cast<float>(m_width) - padding, padding);
+        ImVec2 windowPosPivot = ImVec2(1.0f, 0.0f);  // right top corner (1.0, 0.0)
+
+        ImGui::SetNextWindowPos(windowPos, ImGuiCond_Always, windowPosPivot);
+        ImGui::SetNextWindowBgAlpha(0.35f);  // slightly transparent black background, not blocking 3D scene
+
+        // Make the window invisible and not interactable with the user
+        ImGuiWindowFlags windowFlags =
+            ImGuiWindowFlags_NoDecoration |
+            ImGuiWindowFlags_AlwaysAutoResize |
+            ImGuiWindowFlags_NoSavedSettings |
+            ImGuiWindowFlags_NoFocusOnAppearing |
+            ImGuiWindowFlags_NoNav |
+            ImGuiWindowFlags_NoMove;
+
+        static uint32_t prevDrawCall = 0;
+        uint32_t currDrawCall        = m_renderer.getDrawCall();
+
+        if (ImGui::Begin("HUD", nullptr, windowFlags)) {
+            ImGui::Text("PERFORMANCE");
+            ImGui::Separator();
+            ImGui::Text("FPS       : %.1f", calculateFPS(deltaTime));
+            ImGui::Text("Frame Time: %.2f ms", deltaTime * 1000.0f);
+            ImGui::Text("Draw Call: %d draw calls", currDrawCall - prevDrawCall);
+
+            ImGui::Spacing();
+            ImGui::Text("STATE");
+            ImGui::Separator();
+            const auto& camera = m_scene.getCamera();
+            glm::vec3 pos      = camera->getEye();
+            glm::vec3 target   = camera->getTarget();
+            glm::vec3 front    = glm::normalize(target - pos);
+            ImGui::Text("Camera Position : (%.2f, %.2f, %.2f)", pos.x, pos.y, pos.z);
+            ImGui::Text("Camera FrontVec : (%.2f, %.2f, %.2f)", front.x, front.y, front.z);
+            ImGui::Text("Shadow Mapping : %s", m_renderer.isShadowMapEnabled() ? "On" : "Off");
+            ImGui::Text("Environment IBL : %s", m_renderer.isIBLEnabled() ? "On" : "Off");
+        }
+        ImGui::End();
+
+        prevDrawCall = currDrawCall;
+    }
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+float Application::calculateFPS(float deltaTime) {
+    static float timer    = 0.0f;
+    static int frameCount = 0;
+    static float fps      = 0.0f;
+
+    timer += deltaTime;
+    frameCount++;
+
+    if (timer >= 0.5f) {
+        float nfps = static_cast<float>(frameCount) / timer;
+
+        if (fps == 0.0f) {
+            fps = nfps;
+        } else {
+            fps = (fps * 0.95f + nfps * 0.05f);
+        }
+
+        timer      = 0.0f;
+        frameCount = 0;
+    }
+
+    return fps;
+}
+
+void Application::scrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
+    auto* app = static_cast<Application*>(glfwGetWindowUserPointer(window));
+    if (!app) return;
+
+    auto& camera = app->m_scene.getCamera();
+    if (camera) {
+        camera->zoom(static_cast<float>(yoffset));
+    }
+}
+
+void Application::mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+    auto* app = static_cast<Application*>(glfwGetWindowUserPointer(window));
+    if (!app) return;
+
+    if (button == GLFW_MOUSE_BUTTON_RIGHT) {
+        if (action == GLFW_PRESS) {
+            app->m_isMouseDragging = true;
+
+            double xpos, ypos;
+            glfwGetCursorPos(window, &xpos, &ypos);
+            app->m_lastX = static_cast<float>(xpos);
+            app->m_lastY = static_cast<float>(ypos);
+        } else if (action == GLFW_RELEASE) {
+            app->m_isMouseDragging = false;
+        }
+    }
+}
+
+void Application::cursorPosCallback(GLFWwindow* window, double xpos, double ypos) {
+    auto* app = static_cast<Application*>(glfwGetWindowUserPointer(window));
+    if (!app) return;
+    if (!app->m_isMouseDragging) return;
+
+    float currX   = static_cast<float>(xpos);
+    float currY   = static_cast<float>(ypos);
+    float offsetX = currX - app->m_lastX;
+    float offsetY = app->m_lastY - currY;
+    app->m_lastX  = currX;
+    app->m_lastY  = currY;
+
+    auto& camera = app->m_scene.getCamera();
+    if (camera) {
+        camera->rotate(offsetX, offsetY);
+    }
+}
+
+void Application::frameSizeCallback(GLFWwindow* window, int width, int height) {
+    auto* app = static_cast<Application*>(glfwGetWindowUserPointer(window));
+    if (!app) return;
+    // TODO: resize renderer
 }
 
 }  // namespace tinyrenderer
