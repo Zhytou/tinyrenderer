3a5cc3d4397613e633f4dd4a6ea761a4caac3556
Zhytou <Zhoytou@gmail.com>
2026-06-18 17:11:27 +0800

    add api for hud draw in application

--- include/renderer.hpp
+++ include/renderer.hpp
@@ -32,8 +32,23 @@ class Renderer {
 
     void setup();
     void shutdown();
+    // Convert .hdr skybox to cubemap and bake the IBL environment map
     void prepare(const Scene& scene);
+    // Update ubo/ssbo and bake shadow map
+    void update(const Scene& scene);
+    // Render scene
     void render(const Scene& scene);
+
+    bool isShadowMapEnabled() const { return m_enableShadow; }
+    bool isIBLEnabled() const { return m_enableIBL; }
+    uint32_t getDrawCall() const { return m_drawCall; }
+    void setShadowMap(bool enable) { m_enableShadow = enable; }
+    void setIBL(bool enable) { m_enableIBL = enable; }
+    void setShadowMapSize(uint32_t width, uint32_t height) {
+        m_shadowMapWidth  = width;
+        m_shadowMapHeight = height;
+    }
+    void setSkyboxSize(uint32_t size) { m_skyboxSize = size; }
 
    private:
     // draw mesh
@@ -58,10 +73,11 @@ class Renderer {
     std::unordered_map<std::string, std::string> m_pass2FrameNames;
     std::unordered_map<std::string, uint32_t> m_texture2SlotIndexs;
 
-    bool m_enableIBL = false, m_enableShadow = false;
+    bool m_enableIBL = true, m_enableShadow = false;
     uint32_t m_width = 800, m_height = 600;
     uint32_t m_skyboxSize     = 512;
     uint32_t m_shadowMapWidth = 4096, m_shadowMapHeight = 4096;
+    uint32_t m_drawCall = 0;
 };
 
 }  // namespace tinyrenderer
