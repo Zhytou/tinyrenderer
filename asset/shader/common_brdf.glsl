306b910780a8823159b7f78267e32004c7b9ac84
Zhytou <Zhoytou@gmail.com>
2026-06-19 15:32:44 +0800

    update G_SchlicksmithGGX and remove marco DENOM_EPSILON 1、G_SchlicksmithGGX不在传入roughness，而是直接接受k。它在不同调用中值不一致，比如pbr的brdf计算中，k=(roughness + 1.0) * (roughness + 1.0) / 8.0;而在ibl的brdf积分中计算时，则采用(roughness * roughness) / 2.0。 2、删除宏，改用局部的const变量，进而避免在include时报错。

--- asset/shader/common_brdf.glsl
+++ asset/shader/common_brdf.glsl
@@ -1,10 +1,6 @@
 #ifndef COMMON_BRDF_GLSL
 #define COMMON_BRDF_GLSL
 
-#ifndef DENOM_EPSILON  // Denominator epsilon to avoid division by zero
-#define DENOM_EPSILON 1e-6
-#endif
-
 float D_GGX(float NdotH, float roughness) {
     const float PI = 3.1415926535897932384626;
     float a      = roughness * roughness;
@@ -22,8 +18,7 @@ vec3 F_Schlick(float NdotV, vec3 F0) {
     return F0 + (vec3(1.0) - F0) * pow(1.0 - NdotV, 5.0);
 }
 
-float G_SchlicksmithGGX(float NdotL, float NdotV, float roughness) {
-    float k = (roughness + 1.0) * (roughness + 1.0) / 8.0;
+float G_SchlicksmithGGX(float NdotL, float NdotV, float k) {
     float GL = NdotL / (NdotL * (1.0 - k) + k);
     float GV = NdotV / (NdotV * (1.0 - k) + k);
 
@@ -41,15 +36,17 @@ vec3 BRDF(vec3 L, vec3 V, vec3 N, vec3 F0, vec3 baseColor, float metallic, float
     // D = Normal distribution (Distribution of the microfacets)
     float D = D_GGX(NdotH, roughness);
     // G = Geometric shadowing term (Microfacets shadowing)
-    float G = G_SchlicksmithGGX(NdotL, NdotV, roughness);
+    float k = (roughness + 1.0) * (roughness + 1.0) / 8.0;
+    float G = G_SchlicksmithGGX(NdotL, NdotV, k);
     // F = Fresnel factor (Reflectance depending on angle of incidence)
     vec3 F = F_Schlick(HdotV, F0);
 
     vec3 kS = F;
     vec3 kD = (vec3(1.0) - kS) * (1.0 - metallic);
 
+    const float eps = 1e-6;
     vec3 diffuse = baseColor * NdotL;
-    vec3 specular = D * F * G / (4.0 * NdotL * NdotV + DENOM_EPSILON);
+    vec3 specular = D * F * G / (4.0 * NdotL * NdotV + eps);
 
     return (diffuse + specular) * NdotL;
 }
