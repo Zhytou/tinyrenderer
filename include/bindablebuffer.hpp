e6f25866c70fbb58acea65854657436dfb9c13f9
Zhytou <Zhoytou@gmail.com>
2026-06-22 16:46:39 +0800

    rename buffer type variable as buffer target

--- include/bindablebuffer.hpp
+++ include/bindablebuffer.hpp
@@ -6,18 +6,18 @@ namespace tinyrenderer {
 
 class BindableBuffer : public GraphicBuffer {
    public:
-    BindableBuffer(GLenum type, GLsizeiptr size, const void* data = nullptr) : GraphicBuffer(type, size, data) {}
+    BindableBuffer(GLenum target, GLsizeiptr size, const void* data = nullptr) : GraphicBuffer(target, size, data) {}
     ~BindableBuffer() = default;
 
     // Bind the whole uniform buffer to shader binding point.
     void bind(GLuint slot) const {
-        glBindBufferBase(m_type, slot, m_id);
+        glBindBufferBase(m_target, slot, m_id);
     }
 
     // Bind the subrange of uniform buffer to shader binding point.
     // @para
     void bind(GLuint slot, GLintptr offset, GLsizeiptr size) {
-        glBindBufferRange(m_type, slot, m_id, offset, size);
+        glBindBufferRange(m_target, slot, m_id, offset, size);
     }
 };
 
--- include/graphicbuffer.hpp
+++ include/graphicbuffer.hpp
@@ -5,14 +5,14 @@
 namespace tinyrenderer {
 class GraphicBuffer {
    public:
-    GraphicBuffer(GLenum type, GLsizeiptr size, const void* data = nullptr);
+    GraphicBuffer(GLenum target, GLsizeiptr size, const void* data = nullptr);
     ~GraphicBuffer();
 
     GraphicBuffer(const GraphicBuffer&)            = delete;
     GraphicBuffer& operator=(const GraphicBuffer&) = delete;
 
     GLuint getId() const { return m_id; }
-    GLenum getType() const { return m_type; }
+    GLenum getTarget() const { return m_target; }
     GLsizeiptr getSize() const { return m_size; }
 
     // Upload data to graphic buffer.
@@ -23,7 +23,7 @@ class GraphicBuffer {
 
    protected:
     GLuint m_id       = 0;
-    GLenum m_type     = GL_ARRAY_BUFFER;  // could be vbo/ebo/ubo ...
+    GLenum m_target   = GL_ARRAY_BUFFER;  // could be vbo/ebo/ubo ...
     GLsizeiptr m_size = 0;
 };
 
