4711805a8ce4e0e04056362c7997278ca7b3f432
Zhytou <Zhoytou@gmail.com>
2026-06-29 16:39:25 +0800

    update scene default material load

--- src/scene.cpp
+++ src/scene.cpp
@@ -54,34 +54,21 @@ void Scene::initialize(const std::string& json, ResourceManager& manager) {
             m_models.emplace_back(manager.loadModel(baseDir, modelName));
 
             // default material optional
-            std::map<std::vector<const char*>, const char*> mat2Type = {
-                {{"albedo"}, "vec3"},
-                {{"normal"}, "vec3"},
-                {{"metallic", "roughness", "ambient"}, "float"},
-                {{"albedo_map"}, "string"},
-                {{"normal_map"}, "string"},
-                {{"metallic_map", "roughness_map", "ambient_map"}, "string"},
-            };
             if (modelDoc.HasMember("default_mat")) {
                 auto& matDoc = modelDoc["default_mat"];
-                glm::vec3 albedo = matDoc.HasMember("albedo") ? getVec3(matDoc["albedo"]) : glm::vec3(0.5f);
-                float metallic = matDoc.HasMember("metallic") ? matDoc["metallic"].GetFloat() : 0.0f;
-                float roughness = matDoc.HasMember("roughness") ? matDoc["roughness"].GetFloat() : 0.0f;
-
-                std::string baseMatDir = matDoc.HasMember("base_dir") ? matDoc["base_dir"].GetString() : "";
                 tinyobj::material_t material;
                 material.name = matDoc.HasMember("name") ? matDoc["name"].GetString() : modelName.string() + "_default";
-                material.diffuse[0] = albedo[0];
-                material.diffuse[1] = albedo[1];
-                material.diffuse[2] = albedo[2];
-                material.metallic = metallic;
-                material.roughness = roughness;
+                material.diffuse[0] = matDoc.HasMember("albedo") ? matDoc["albedo"][0].GetFloat() : 0.5f;
+                material.diffuse[1] = matDoc.HasMember("albedo") ? matDoc["albedo"][1].GetFloat() : 0.5f;
+                material.diffuse[2] = matDoc.HasMember("albedo") ? matDoc["albedo"][2].GetFloat() : 0.5f;
+                material.metallic = matDoc.HasMember("metallic") ? matDoc["metallic"].GetFloat() : 0.0f;;
+                material.roughness = matDoc.HasMember("roughness") ? matDoc["roughness"].GetFloat() : 0.0f;
                 material.diffuse_texname = matDoc.HasMember("albedo_map") ? matDoc["albedo_map"].GetString() : "";
                 material.normal_texname = matDoc.HasMember("normal_map") ? matDoc["normal_map"].GetString() : "";
                 material.metallic_texname = matDoc.HasMember("metallic_map") ? matDoc["metallic_map"].GetString() : "";
                 material.roughness_texname = matDoc.HasMember("roughness_map") ? matDoc["roughness_map"].GetString() : "";
                 material.ambient_texname = matDoc.HasMember("ambient_map") ? matDoc["ambient_map"].GetString() : "";
-
+                std::string baseMatDir = matDoc.HasMember("base_dir") ? matDoc["base_dir"].GetString() : "";
                 auto nmaterial = manager.loadMaterial(baseMatDir, material);
                 m_models.back()->setDefaultMaterial(nmaterial);
             }
@@ -102,7 +89,7 @@ void Scene::initialize(const std::string& json, ResourceManager& manager) {
         }
     }
 
-        // skybox(environment map)
+    // skybox(environment map)
     if (doc.HasMember("skybox")) {
         if (doc["skybox"].HasMember("cubemap")) {
             std::vector<fs::path> imagePaths;
