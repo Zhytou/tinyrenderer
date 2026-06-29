9b57e4de4b40d37abd785911019a22d94fd6322d
Zhytou <Zhoytou@gmail.com>
2026-06-29 22:14:20 +0800

    update

--- include/renderitem.hpp
+++ include/renderitem.hpp
@@ -16,13 +16,13 @@ namespace tinyglrenderer {
  * for a single primitive draw call. Collected dynamically into sorting queues for rendering.
  */
 struct RenderItem {
-    const std::unique_ptr<Mesh>& mesh;
+    const std::shared_ptr<Mesh>& mesh;
     const std::shared_ptr<Material>& material;
 
-    uint32_t ioffset = 0;    // vertex input data offset of vbo/ibo in vertex count
-    uint32_t length  = 0;    // vertex input data length of vbo/ibo in vertex count
-    float distance   = 0.f;  // distance to the camera(for transparent objects sorting)
-    uint32_t uoffset = 0;    // model block index of ubo in bytes
+    uint ioffset = 0;   // vertex input data offset of vbo/ibo in vertex count
+    uint length  = 0;   // vertex input data length of vbo/ibo in vertex count
+    float distance   = 0.f; // distance to the camera(for transparent objects sorting)
+    uint uoffset = 0;   // model block index of ubo in bytes
 };
 
-}  // namespace tinyglrenderer
+} // namespace tinyglrenderer
